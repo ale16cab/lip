f4d8e592d62c60214cdee411f41c313a8df3fb30
ale16cab <alessandroocabras@gmail.com>
2025-11-28 10:46:15 +0100

    boolexpr

--- expr/boolexpr/test/boolexpr.ml
+++ expr/boolexpr/test/boolexpr.ml
@@ -5,8 +5,8 @@ let test_eval expr exp_result =
 
 (* ### Unit tests for task 4 *)
 
-let%test "test_eval_1" = failwith "TODO"
+let%test "test_eval_1" = test_eval "if true then false else true" false
 
 (* ### Unit tests for task 5 *)
 
-let%test "test_trace1_1" = failwith "TODO"
+(* let%test "test_trace1_1" = failwith "TODO" *)
