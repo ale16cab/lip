ba67cccd348a3d501e7cebe7aa9e5c3b808b93bf
ale16cab <alessandroocabras@gmail.com>
2025-12-17 17:58:41 +0100

    adder

--- basics/adder/lib/adder.ml
+++ basics/adder/lib/adder.ml
@@ -1,4 +1,8 @@
 (* val addlist : int list -> int *)
 (* addlist l adds the element of the list of integers l *)
 
-let addlist l = 0 (* replace 0 with actual code *)
+let addlist l : int = 
+  let rec aux list = match list with
+    [] -> 0
+    | x::xs -> x + aux xs 
+  in aux l;;
--- basics/adder/test/addertest.ml
+++ basics/adder/test/addertest.ml
@@ -4,3 +4,6 @@ let%test _ = addlist [] = 0
 let%test _ = addlist [3] = 3
 let%test _ = addlist [1;2] = 3
 let%test _ = addlist [1;2;3] = 6
+let%test _ = addlist [11; 15; 24] = 50
+let%test _ = addlist [7;16;9;27] = 59
+let%test _ = addlist [836; 111; 2222] = 3169
